d835c509e88f078a111417e97e0118d44893e5b9
wilcorrea <wilcorrea@gmail.com>
2016-09-03 18:02:11 -0300

    VAI DANADO

--- exemplo3/layout/printer.php
+++ exemplo3/layout/printer.php
@@ -7,12 +7,8 @@ $array[2] = $variavel3;
 $array[3] = $variavel4;
 $array[4] = $variavel5;
 
-echo "<pre>";
-var_dump($array);
-echo "</pre>";
-
 foreach($array as $indice => $valor) {
   echo "<pre>";
-  var_dump([$indice, $valor]);
+  var_dump(array($indice, $valor));
   echo "</pre>";
 }
